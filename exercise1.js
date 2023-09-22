f7eefe8f50e990acc6d76fba2533a206af985bc8
StudantMark <G00351394@atu.ie>
2023-09-22 13:59:27 +0000

    first commit

---
+++ exercise1.js
@@ -0,0 +1,24 @@
+console.log("Hello world");
+
+function SayHello(){
+    console.log("Hello!");
+}
+SayHello();
+
+const myFunction = function SayHi(){
+    console.log("Hi");
+}
+
+myFunction();
+
+const myHiFunction = function(){
+    console.log("Good Day");
+}
+
+myHiFunction();
+
+const myComponent = ()=>{
+    console.log("Bye!")
+}
+
+myComponent();
